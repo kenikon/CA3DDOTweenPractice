47bf9d27f112dece0ace0071309566a26d17e8be
Kenan Alizade <kenelisan@gmail.com>
2022-10-19 11:48:16 +0400

    First commit

---
+++ CA3DDOTweenPractice/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] FloatingJoystick joyStick;
+    [Header("Movement Speed")]
+    [SerializeField] float moveSpeed;
+    float Horizontal;
+    float Vertical;
+    Vector3 direction;
+    Rigidbody rb;
+    float currentTurnAngle;
+    float smoothTurnTime = 0.1f;
+    [Header("Detect & Collect")]
+    Collider[] collidersCollect;
+    Collider[] collidersStore;
+    [SerializeField] Transform detectTransform;
+    [SerializeField] float detectionRange = 1;
+    [SerializeField] LayerMask layerCollect;
+    [SerializeField] LayerMask layerStore;
+    [SerializeField] Transform holdTransform;
+    [SerializeField] Transform storeTransform;
+    [SerializeField] int itemCount = 0;
+    [SerializeField] float itemDistanceBetween = 0.5f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = new Color(0.7f,0.0f,0.0f,0.7f);
+        Gizmos.DrawSphere(detectTransform.position, detectionRange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        collidersCollect = Physics.OverlapSphere(detectTransform.position, detectionRange, layerCollect);
+        foreach (var hit in collidersCollect)
+        {
+            if (hit.CompareTag("Collectable")) {
+                hit.tag = "Collected";
+                hit.transform.parent = holdTransform;
+
+                var seq = DOTween.Sequence();
+
+                seq.Append(hit.transform.DOLocalJump(new Vector3(0, itemCount * itemDistanceBetween), 2, 1, 0.3f)
+                .Join(hit.transform.DOScale(1.25f, 0.1f))
+                .Insert(0.1f, hit.transform.DOScale(0.3f, 0.2f)));
+                seq.AppendCallback(() => {
+                    hit.transform.localRotation = Quaternion.Euler(0,0,0);
+                });
+
+                itemCount++;
+            }
+        }
+
+        collidersStore = Physics.OverlapSphere(detectTransform.position, detectionRange, layerStore);
+        foreach (var hit in collidersStore)
+        {
+            if (hit.CompareTag("Storage") && itemCount > 0) {
+                // hit.tag = "Stored";
+                GameObject col = GameObject.FindGameObjectWithTag("Collected");
+                col.transform.parent = storeTransform;
+                col.tag = "Stored";
+                itemCount--;
+
+                var seq = DOTween.Sequence();
+
+                seq.Append(col.transform.DOLocalJump(new Vector3(0, itemCount * itemDistanceBetween),2, 1, 0.3f)
+                .Join(col.transform.DOScale(1.25f, 0.1f))
+                .Insert(0.1f, col.transform.DOScale(0.3f, 0.2f)));
+                seq.AppendCallback(() => {
+                    col.transform.localRotation = Quaternion.identity;
+                });
+            }
+        }
+    }
+
+    private void FixedUpdate() {
+        Horizontal = joyStick.Horizontal;
+        Vertical = joyStick.Vertical;
+
+        direction = new Vector3(Horizontal, 0, Vertical);
+        if (direction.magnitude > 0.01f) {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float turnAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref currentTurnAngle, smoothTurnTime);
+
+            transform.rotation = Quaternion.Euler(0, turnAngle, 0);
+
+            rb.MovePosition(transform.position + (direction * moveSpeed * Time.deltaTime));
+        }
+    }
+}
